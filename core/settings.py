83b72609f75ed405f73a0759470839fcf4b047af
proj7436 <umbalaavc16@gmail.com>
2024-04-21 18:43:26 +0700

    upload

--- core/settings.py
+++ core/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%-9eysoxyic3+sruh3n3@$@e6u5xl855&!(49_4q4p5)g!_+c)'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+import django
+django.setup()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
